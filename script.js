a2622a30071a72b200f45385fde71ffdfe000061
Vehinys <albert.lecomte1989@gmail.com>
2026-02-01 17:22:21 +0100

    modification

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 let mybutton = document.getElementById("scrollToTopBtn");
 
-        window.onscroll = function() {
-            scrollFunction();
-        };
+window.onscroll = function () {
+  scrollFunction();
+};
 
-        function scrollFunction() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                mybutton.style.display = "block";
-            } else {
-                mybutton.style.display = "none";
-            }
-        }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-        mybutton.onclick = function() {
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-        };
+mybutton.onclick = function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
 
-    // Menu burger
+// Sticky Header
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
-        const button = document.querySelector('.burger');
-        const nav = document.querySelector('.navbar-links');
-         
-            button.addEventListener('click', () => {
-                nav.classList.toggle('active');
-            });
+// Menu burger
+const button = document.querySelector(".burger");
+const nav = document.querySelector(".navbar-links");
+const navLinks = document.querySelectorAll(".navbar-link");
 
-            
+button.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  button.classList.toggle("toggle"); // For animation if needed
+});
+
+// Close menu when link is clicked
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+    button.classList.remove("toggle");
+  });
+});
